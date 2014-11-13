f209d098cad0e3952a644adcd2bafe3c9e7d7446
unknown <vikeer@gmail.com>
2014-11-13 09:32:59 +0200

    Update config.php

---
+++ push/lib/config.php
@@ -0,0 +1,20 @@
+<?php
+/**
+ * config.php
+ *
+ * @author Vikeer Jaichand
+ *
+ * Basic configuration for the project.
+ *
+ * Database config, Google Cloud Messaging api keys etc.
+ *
+ */
+
+// -- Database configs
+define("DB_HOST", "");
+define("DB_USER", "");
+define("DB_PASS", "");
+define("DB_NAME", "");
+
+// -- Google API Key / Token for server
+define("GOOGLE_API_TOKEN", "Add GOOGLE API TOKEN HERE");
